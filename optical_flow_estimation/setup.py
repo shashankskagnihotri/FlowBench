3f2125396fec7859cfd75f0e440d41252340a3a8
Jonas Jakubsassa <jakubassa@posteo.de>
2025-06-03 14:13:22 +0200

    missing dependency

--- optical_flow_estimation/setup.py
+++ optical_flow_estimation/setup.py
@@ -18,6 +18,7 @@ setup(
         "torch-scatter==2.1.2",
         "torchaudio==2.7.0+cu118",
         "torchvision==0.22.0+cu118",
+        "matplotlib==3.10.3"
     ],
     python_requires='>=3.10, <3.11',
     license='MIT',
