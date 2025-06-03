317702bb681a67632fd473fc49a138d0478b01a3
Jonas Jakubsassa <jakubassa@posteo.de>
2025-06-03 14:02:34 +0200

    avoid using system pip

--- optical_flow_estimation/install.sh
+++ optical_flow_estimation/install.sh
@@ -2,20 +2,20 @@
 set -e
 
 echo "Installing PyTorch 2.7.0 + CUDA 11.8..."
-pip install torch==2.7.0+cu118 torchvision==0.22.0+cu118 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu118
+python -m pip install torch==2.7.0+cu118 torchvision==0.22.0+cu118 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu118
 
 echo "Installing torch-scatter with PyG wheels..."
-pip install torch-scatter -f https://data.pyg.org/whl/torch-2.7.0+cu118.html
+python -m pip install torch-scatter -f https://data.pyg.org/whl/torch-2.7.0+cu118.html
 
 echo "Installing imagecorruptions from GitHub..."
-pip install git+https://github.com/bethgelab/imagecorruptions.git
+python -m pip install git+https://github.com/bethgelab/imagecorruptions.git
 
 echo "Installing spatial-correlation-sampler from GitHub..."
-pip install git+https://github.com/ClementPinard/Pytorch-Correlation-extension.git
+python -m pip install git+https://github.com/ClementPinard/Pytorch-Correlation-extension.git
 
 echo "Installing local ptlflow package..."
 cd ptlflow
-pip install .
+python -m pip install .
 
 echo "Installing alt_cuda_corr extensiocondn..."
 cd ptlflow/utils/external/alt_cuda_corr/
