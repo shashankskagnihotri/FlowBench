4fb7263f71bbef5f63cd43ab7d9afc6b91d6fbea
Jonas Jakubsassa <jakubassa@posteo.de>
2025-06-11 12:13:03 +0200

    avoid cirucular import

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,4 +1,1 @@
-from . import apgd, tdcc, bim_bgd_cospgd, common_corruptions, fab, fgsm, pcfa, weather
 
-
-__all__ = ["apgd", "tdcc", "bim_bgd_cospgd", "common_corruptions", "fab", "fgsm", "pcfa", "weather"]
